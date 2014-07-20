cbcf046c5480e18471168407ef0e6644cfe491a9
masroorhasan <masroor.hasan.n@gmail.com>
2014-07-20 18:08:46 -0400

    full tripunit test cases

--- client/client.c
+++ client/client.c
@@ -14,46 +14,124 @@
 
 #include "ece454_fs.h"
 
+void printBuf(char *buf, int size) {
+    /* Should match the output from od -x */
+    int i;
+    for(i = 0; i < size; ) {
+        if(i%16 == 0) {
+            printf("%08o ", i);
+        }
+
+        int j;
+        for(j = 0; j < 16;) {
+            int k;
+            for(k = 0; k < 2; k++) {
+                if(i+j+(1-k) < size) {
+                    printf("%02x", (unsigned char)(buf[i+j+(1-k)]));
+                }
+            }
+
+            printf(" ");
+            j += k;
+        }
+
+        printf("\n");
+        i += j;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if(argc < 3) {
-	      printf("Usage: %s <Server IP> <Server Port>\n", argv[0]);
-	      return 0;
+    if(argc < 4) {
+        fprintf(stderr, "usage: %s <srv-ip/name> <srv-port> <local dir name>\n", argv[0]);
+        exit(1);
     }
 
-    fsMount(argv[1], atoi(argv[2]), "leaf");
+    char *dirname = argv[3];
+    printf("fsMount(): %d\n", fsMount(argv[1], atoi(argv[2]), dirname));
+    FSDIR *fd = fsOpenDir(dirname);
+    if(fd == NULL) {
+        perror("fsOpenDir"); exit(1);
+    }
 
-    int fdr = fsOpen("leaf/open.txt", 1);
-    printf("Opened leaf/open.txt FD: %i\n", fdr);
+    struct fsDirent *fdent = NULL;
+    for(fdent = fsReadDir(fd); fdent != NULL; fdent = fsReadDir(fd)) {
+        printf("\t %s, %d\n", fdent->entName, (int)(fdent->entType));
+    }
 
-    char buff[16];
-    int r_return;
-    r_return = fsRead(fdr, (void *)buff, 10);
-    printf("bytes read %i into buffer %s\n", r_return, buff);
+    if(errno != 0) {
+        perror("fsReadDir");
+    }
 
-    // int fdw = fsOpen("leaf/one/test1.txt", 1);
-    // printf("Opened leaf/one/test1.txt FD: %i\n", fdw);
+    printf("fsCloseDir(): %d\n", fsCloseDir(fd));
 
-    
-    // char *buffer = "W1";
-    // int w_return;
-    // w_return = fsWrite(fdw, buffer, strlen(buffer) + 1);
+    int ff = open("/dev/urandom", 0);
+    if(ff < 0) {
+        perror("open(/dev/urandom)"); exit(1);
+    }
+    else printf("open(): %d\n", ff);
 
-    // Perform fsread again
+    char fname[256];
+    sprintf(fname, "%s/", dirname);
+    if(read(ff, (void *)(fname+strlen(dirname)+1), 10) < 0) {
+        perror("read(/dev/urandom)"); exit(1);
+    }
 
-    // int c_return = fsClose(fdw);
+    int i;
+    for(i = 0; i < 10; i++) {
+        //printf("%d\n", ((unsigned char)(fname[i]))%26);
+        fname[i+strlen(dirname)+1] = ((unsigned char)(fname[i+strlen(dirname)+1]))%26 + 'a';
+    }
+    fname[10+strlen(dirname)+1] = (char)0;
+    printf("Filename to write: %s\n", (char *)fname);
 
-    // char *file_to_remove = "leaf/test.txt";
-    // int remove_ret = fsRemove(file_to_remove);
-    // printf("remove ret value %i\n", remove_ret);
+    char buf[256];
+    if(read(ff, (void *)buf, 256) < 0) {
+        perror("read(2)"); exit(1);
+    }
 
-    // char *file_to_remove = "leaf/one/delete/delete.txt";
-    // int remove_ret = fsRemove(file_to_remove);
-    // printf("remove ret value %i\n", remove_ret);
+    printBuf(buf, 256);
 
-    // char *dir_to_remove = "leaf/one/delete";
-    // remove_ret = fsRemove(dir_to_remove);
-    // printf("remove ret value %i\n", remove_ret);    
+    printf("close(): %d\n", close(ff));
 
-    fsUnmount("leaf");
+    ff = fsOpen(fname, 1);
+    if(ff < 0) {
+        perror("fsOpen(write)"); exit(1);
+    }
+
+    if(fsWrite(ff, buf, 256) < 256) {
+        fprintf(stderr, "fsWrite() wrote fewer than 256\n");
+    }
+
+    if(fsClose(ff) < 0) {
+        perror("fsClose"); exit(1);
+    }
+
+    char readbuf[256];
+    if((ff = fsOpen(fname, 0)) < 0) {
+        perror("fsOpen(read)"); exit(1);
+    }
+
+    int readcount = -1;
+
+    if((readcount = fsRead(ff, readbuf, 256)) < 256) {
+        fprintf(stderr, "fsRead() read fewer than 256\n");
+    }
+
+    if(memcmp(readbuf, buf, readcount)) {
+        fprintf(stderr, "return buf from fsRead() differs from data written!\n");
+    } else {
+        printf("fsread(): return buf identical to data written upto %d bytes.\n", readcount);
+    }
+
+    if(fsClose(ff) < 0) {
+        perror("fsClose"); exit(1);
+    }
+
+    printf("fsRemove(%s): %d\n", fname, fsRemove(fname));
+
+    if(fsUnmount(dirname) < 0) {
+        perror("fsUnmount"); exit(1);
+    }
+
     return 0;
 }

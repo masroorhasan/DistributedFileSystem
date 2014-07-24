dfc10e2e0b855c4359e6828466126ff39f0a8366
Warren Smith <sharpsw@gmail.com>
2014-07-23 22:58:10 -0400

    Wrote concurrent client test case.

---
+++ client/concurrent_client_2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+#include <time.h>
+#include <string.h>
+
+#include "ece454_fs.h"
+
+void timestamp() {
+    time_t ltime;
+    ltime=time(NULL);
+    printf("\n%s",asctime( localtime(&ltime) ) );
+}
+
+int main(int argc, char *argv[]) {
+
+    fsMount(argv[1], atoi(argv[2]), "root");
+
+    timestamp();
+    printf("Attempting to open one.txt in grandfather directory\n");
+    int fd = fsOpen("root/test.txt", 1);
+
+    timestamp();
+    printf("Opened one.txt with fd: %i\n", fd);
+
+    timestamp();
+    printf("Issuing writes to file\n");
+    int w_return;
+    char *buffer = "W4";
+    w_return = fsWrite(fd, buffer, strlen(buffer) + 1);
+    printf("Write one code: %i\n", w_return);
+
+    buffer = "W5";
+    w_return = fsWrite(fd, buffer, strlen(buffer) + 1);
+    printf("Write two code: %i\n", w_return);
+
+    timestamp();
+    printf("Closing one.txt\n");
+    int close_fd = fsClose(fd);
+
+    timestamp();
+    printf("Closed with code: %i\n", close_fd);
+}
